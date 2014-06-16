91b842c8515b0794b0869e73889f1466984d39d1
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-16 22:09:34 +0200

    Update run.R

--- src/main/app-resources/sar-stack/run.R
+++ src/main/app-resources/sar-stack/run.R
@@ -9,75 +9,14 @@ library("RCurl")
 library("sp")
 library("rgeos") 
 
+source("/application/sar-stack/libexec/lib.R")
+
 # ERS-1, ERS-2, Envisat number of cycles
 max.cycles <- list("ERS-1"=200, "ERS-2"=200, "ENVISAT"=110)
 
 # get the bbox
 bbox <- rciop.getparam("bbox")
 
-# a function to get the mission: ERS-1, ERS-2 or ENVISAT
-GetMissionInfo <- function (osd.description) {
-  
-  cat.res <- getURL(str_replace(osd.description, "description", "rdf"))
-  
-  mission <- xpathApply(xmlParse(cat.res), "//dclite4g:Series/eop:platform", xmlValue)
-  
-  title <- xpathApply(xmlParse(cat.res), "//dclite4g:Series/dc:title", xmlValue)
-  
-  identifier <- xpathApply(xmlParse(cat.res), "//dclite4g:Series/dc:identifier", xmlValue)
-  
-  return(list(identifier=identifier, mission=mission, title=title))
-}
-
-GetCoverage <- function(osd.description, response.type, q) {
-
-  # get the queryables
-  q <- GetOSQueryables(osd.description, response.type)
-  
-  # fill the queryables values
-  q$value[q$type == "geo:box"] <- bbox
-  q$value[q$type == "count"] <- 900
-  
-  # query the catalogue 
-  query.res <- Query(osd.description, response.type, q) 
-  
-  # from the catalogue response, extract the datasets
-  dataset <- xmlToDataFrame(nodes = getNodeSet(xmlParse(query.res), 
-    "//dclite4g:DataSet"), stringsAsFactors = FALSE)
-  
-  # get the list of relative orbits within a cycle covering the AOI
-  rel.orbit <- unique(dataset$wrsLongitudeGrid)
-  
-  # for each relative orbit, get the cycles where that relative orbit is available on the catalogue 
-  cycle <- lapply(rel.orbit, function(x) {
-   
-    subset.track <- dataset[dataset$wrsLongitudeGrid == x,]
-    
-    list(relative.orbit=x, cycles=rev(unique(subset.track$cycle)))
-   })
-  
-  # create a list with the sequence of the satelite cycles
-  cycle.list <- c(seq(1:as.numeric(max.cycles[satelite])))
-  
-  # create an empty data frame
-  my.df <- data.frame(matrix(NA, nrow = as.numeric(max.cycles[satelite]), ncol = 0))
-
-  # fill the data frame with logical values
-  for (index in seq(1, length(rel.orbit))) {
-    both <- union(cycle.list, as.numeric(cycle[[index]]$cycles))
-    d <- both %in% as.numeric(cycle[[index]]$cycles)
-    my.df <- cbind(my.df, d)
-  }
-  
-  # set the names of the data frame
-  rownames(my.df) <- paste("cycle", 1:as.numeric(max.cycles[satelite]), sep="_")
-  colnames(my.df) <- rel.orbit
-
-  return(my.df)
-  
-}  
-  
-  
 # set the response type for the catalogue queries
 response.type <- "application/rdf+xml"
 
